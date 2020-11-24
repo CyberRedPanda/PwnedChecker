251962d2391367c2c0a4f6b29df58c5f0d7d282b
CyberRedPanda <67606389+CyberRedPanda@users.noreply.github.com>
2020-11-24 14:28:01 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://haveibeenpwned.com/") 
+time.sleep(5)
+
+with open("pwned.txt") as f:
+	for line in f:
+		line = str(line.strip())
+		input_field = driver.find_element(By.XPATH, '//*[@id="Account"]')
+		input_field.send_keys(line)
+		time.sleep(2)
+		enter = driver.find_element(By.XPATH, '//*[@id="searchPwnage"]')
+		enter.click()
+		time.sleep(2)
+		response = driver.find_element_by_class_name("pwnTitle").text
+		if "Good news — no pwnage found!" in response:
+			print(str(line) + " CLEAN!" )
+		else:
+			print(str(line) + " PWNED!" )
+		driver.get("https://haveibeenpwned.com/")		
+
+
+
+
